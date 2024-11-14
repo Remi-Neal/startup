09d21e8ddea1917c1d4eb60ffafe81c810e63791
Remi <90064274+Remi-Neal@users.noreply.github.com>
2024-11-14 13:10:44 -0700

    Updated app.jsx to use react-router-dom to route each page using the nav bar

--- src/app.jsx
+++ src/app.jsx
@@ -1,49 +1,74 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import Home from './home/home';
+import History from './history/history';
+import BookFinder from './book_finder/book_finder';
+import MessageBoard from './message_board/message_board';
+import Login from './login/login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
   return (
     <>
-        <header className=""> 
+    <BrowserRouter>
+        <header> 
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
-                <a className="navbar-brand" href="#">Codex Vivit</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="history.html">History</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Book List
-                            </a>
-                            <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="https://archive.org/">Internet Archive</a></li>
-                            <li><a className="dropdown-item" href="https://www.abebooks.com/">AbleBooks</a></li>
-                            <li><hr className="dropdown-divider"></hr></li>
-                            <li><a className="dropdown-item" href="book_finder.html">All</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="message_board.html">Message Board</a>
-                        </li>
-                    </ul>
-                    <form className="d-flex" role="search">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                    <a className="nav-link" href="login.htm">Login</a>
-                </div>
+                    <NavLink className="navbar-brand" to="home">Codex Vivit</NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink className="nav-link active" aria-current="home" to="home">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="history">History</NavLink>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <NavLink className="nav-link dropdown-toggle" to="book_finder" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Book List
+                                </NavLink>
+                                <ul className="dropdown-menu">
+                                <li>
+                                    <NavLink className="dropdown-item" to="https://archive.org/">Internet Archive</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className="dropdown-item" to="https://www.abebooks.com/">AbleBooks</NavLink>
+                                </li>
+                                <li>
+                                    <hr className="dropdown-divider"></hr>
+                                </li>
+                                <li>
+                                    <NavLink className="dropdown-item" to="book_finder">All</NavLink>
+                                </li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="message_board">Message Board</NavLink>
+                            </li>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                        <NavLink className="nav-link" to="login">Login</NavLink>
+                    </div>
                 </div>
             </nav>
         </header>
+
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='home' element={<Home />} />
+            <Route path='history' element={<History />} />
+            <Route path='book_finder' element={<BookFinder />} />
+            <Route path='message_board' element={<MessageBoard />} />
+            <Route path='login' element={<Login />} />
+            <Route path='*' element={<Home />} />
+        </Routes>
 
         <footer className='footer fixed-bottom bg-light'>
             <p><strong>About: </strong>This site was originally created for the completion of BYU's C S 260: Web Programming course</p>
@@ -52,6 +77,7 @@ export default function App() {
                 <p><strong>Repo: </strong><a href="https://github.com/Remi-Neal/startup">GitHub</a></p>
             </div>
         </footer>
+    </BrowserRouter>
     </>
   );
 }
--- src/message_board/message_board.jsx
+++ src/message_board/message_board.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function MessageBoard () {
+export default function MessageBoard () {
     return (
         <>
             <main>
